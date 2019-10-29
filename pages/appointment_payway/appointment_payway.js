f518fff13c05e6f781adffb8849fd801b0bac2b5
chenwei <chenwei2889@gmail.com>
2019-10-29 17:41:06 +0800

    20191029payway

---
+++ pages/appointment_payway/appointment_payway.js
@@ -0,0 +1,81 @@
+// pages/appointment_payway/appointment_payway.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    parameter: {
+      'navbar': '1',
+      'return': '1',
+      'title': '选择支付方式',
+      'color': true,
+      'class': '0'
+    },
+    Show: false
+  },
+
+  choosemembercard() {
+    this.setData({
+      Show: true
+    })
+  },
+  onClose() {
+    this.setData({ Show: false });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
